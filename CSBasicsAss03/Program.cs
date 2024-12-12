8f1bcd3752ffa1bb4f51f2f43572f38318dc3d3c
Rania Mahmoud <Rania.Mahm555@gmail.com>
2024-12-12 09:27:37 +0200

    add question1

--- CSBasicsAss03/Program.cs
+++ CSBasicsAss03/Program.cs
@@ -4,6 +4,25 @@
     {
 
 
+        #region 1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+
+        //int num;
+        //Console.WriteLine("enter the number ");
+        //bool check = int.TryParse(Console.ReadLine(), out num);
+
+        //if (check)
+        //{
+        //    if (num % 3 == 0 && num % 4 == 0)// short circit 
+        //        Console.WriteLine("Yes");
+        //    else
+        //        Console.WriteLine("No");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("You entered invalid input , you  must enter number");
+        //}
+
+        #endregion
 
 
 
